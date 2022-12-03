3c8b60a04c0b542927ed9b2d8b2012034a711580
rohsik2 <rohsik2@gmail.com>
2022-12-03 16:42:37 +0900

    feat: 플랭크 관련 기능 개발 및 플랭크 점수 인식 알고리즘 개발

---
+++ Assets/Scripts/MiniGame/Plank/PlankMainController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using UnityEngine;
+
+public class PlankMainController : MonoBehaviour
+{
+    public WifiSessionController wifiController;
+    public PlankPlayer player; // 현재 게임을 진행중인 플레이어.
+    public GameObject shield;
+    public GameObject hairband;
+    
+    private float time;
+
+    private long score;
+
+    private bool isStart;
+    private bool doMore;
+    public void Start()
+    {
+        isStart = false;
+        doMore = false;
+    }
+
+    public void Update()
+    {
+        int posScore = player.GetPositionScore();
+
+        if (!isStart && posScore > 4 && doMore)
+        {
+            isStart = true;
+        }
+        
+        //TODO : unity Chan 의 헤어벤드를 가지고 쉴드를 트래킹 하길 원함. 
+        if (posScore > 0)
+        {
+            print(shield.transform.position);
+            shield.transform.position = hairband.transform.position + Vector3.up * 0.01f;
+            shield.SetActive(true);
+        }
+        else
+        {
+            isStart = false;
+            doMore = false;
+        }
+        if(isStart)
+        {
+            time += Time.deltaTime;
+        }
+        score += posScore;
+        Debug.Log(score);
+    }
+
+    public long getPlayerScore()
+    {
+        return score;
+    }
+
+    public float getPlayerTime()
+    {
+        return time;
+    }
+    
+}
---
+++ Assets/Scripts/MiniGame/Plank/PlankPlayer.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class PlankPlayer : MonoBehaviour
+{
+    public VNectModel model;
+    
+    public float GetPositionVectorMagnitude()
+    {
+        var modelJointPoints = model.JointPoints;
+        var head = modelJointPoints[PositionIndex.head.Int()];
+        var hip = modelJointPoints[PositionIndex.hip.Int()];
+        var foot = (modelJointPoints[PositionIndex.rFoot.Int()].Pos3D + modelJointPoints[PositionIndex.lFoot.Int()].Pos3D) /2 ;
+
+        Vector3 hip_to_head = head.Pos3D - hip.Pos3D;
+        Vector3 hip_to_foot = foot - hip.Pos3D;
+
+        hip_to_head = hip_to_head / hip_to_head.magnitude;
+        hip_to_foot = hip_to_foot / hip_to_foot.magnitude;
+
+        var result = (hip_to_head + hip_to_foot).magnitude;
+        return result;
+    }
+
+
+    public int GetPositionScore()
+    {
+        float mag = GetPositionVectorMagnitude();
+        float temp = Mathf.Abs(mag - 0.3f);
+        if (temp < 0.15)
+            return 5;
+        if (temp < 0.3)
+            return 4;
+        if (temp < 0.45)
+            return 3;
+        if (temp < 0.6)
+            return 2;
+        return 0;
+
+    }
+    
+    public void Start()
+    {
+        
+    }
+
+    public void Update()
+    {
+        
+    }
+}
