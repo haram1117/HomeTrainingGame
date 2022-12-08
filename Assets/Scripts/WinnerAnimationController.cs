f97eefe7c00c84dcff3e0fae85b4a9268d0e35a5
HaramPark <haram1117@khu.ac.kr>
2022-12-08 22:27:52 +0900

    feat: Winner Animation

---
+++ Assets/Scripts/WinnerAnimationController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinnerAnimationController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private GameObject girl;
+    [SerializeField] private GameObject boy;
+
+    void Start()
+    {
+
+        //girl.GetComponent<Animator>().SetTrigger()
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
